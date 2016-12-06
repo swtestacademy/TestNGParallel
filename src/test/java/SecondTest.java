566b1a8162405d53210cb5cf2621e56e5ada1e0c
SW Test Academy <swtestacademy@gmail.com>
2016-12-06 20:14:26 +0300

    First changed to Second.

--- src/test/java/SecondTest.java
+++ src/test/java/SecondTest.java
@@ -8,10 +8,10 @@ public class SecondTest extends TestBase{
 
     @Test
     public void secondTest() throws Exception {
-        System.out.println("First Test Started!");
+        System.out.println("Second Test Started!");
         getDriver().navigate().to("http://www.google.com");
         System.out.println("Page title is: " + getDriver().getTitle());
         Assert.assertEquals("Google", getDriver().getTitle());
-        System.out.println("First Test Ended!");
+        System.out.println("Second Test Ended!");
     }
 }
