d5d623942d362f4fc2a69707f2d10a7dd2b85184
SW Test Academy <swtestacademy@gmail.com>
2016-12-24 23:51:52 +0300

    minor changes.

--- src/test/java/LocalDriverFactory.java
+++ src/test/java/LocalDriverFactory.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 class LocalDriverFactory {
     static WebDriver createInstance (String browserName) {
--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -13,7 +13,7 @@ public class Test1 {
 
     private void invokeBrowser(String url) {
         System.out.println("Thread id = " + Thread.currentThread().getId());
-        System.out.println("Hashcode of webDriver instance = " + LocalDriverManager.getDriver().hashCode());
+        System.out.println("Hashcode of webDriver instance = " + LocalDriverManager.getDriver().hashCode() );
         LocalDriverManager.getDriver().get(url);
     }
 }
