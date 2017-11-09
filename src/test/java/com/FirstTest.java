8064a6e44ef0ccdd7f885e8a363bf4ad8ac35c22
SW Test Academy <swtestacademy@gmail.com>
2017-11-10 01:19:28 +0300

    minor changes.

--- src/test/java/com/FirstTest.java
+++ src/test/java/com/FirstTest.java
@@ -9,43 +9,39 @@ import org.testng.annotations.Test;
 public class FirstTest extends TestBase {
 
     @Test
-    public synchronized void GOOGLE0() throws Exception {
+    public void GOOGLE0() throws Exception {
         System.out.println("Google0 Test Started! " + Thread.currentThread().getId());
         driver.navigate().to("http://www.google.com");
         System.out.println("Google0 Test's Page title is: " + driver.getTitle() + " " + Thread.currentThread().getId());
         Assert.assertEquals(driver.getTitle(), "Google");
         System.out.println("Google0 Test Ended! " + Thread.currentThread().getId());
-
     }
 
     @Test
-    public  void GOOGLE2() throws Exception {
+    public void GOOGLE2() throws Exception {
         System.out.println("Google2 Test Started! " + Thread.currentThread().getId());
         driver.navigate().to("http://www.google.com");
         System.out.println("Google2 Test's Page title is: " + driver.getTitle() + " " + Thread.currentThread().getId());
         Assert.assertEquals(driver.getTitle(), "Google");
         System.out.println("Google2 Test Ended! " + Thread.currentThread().getId());
-
     }
 
     @Test
-    public  void GOOGLE3() throws Exception {
+    public void GOOGLE3() throws Exception {
         System.out.println("Google3 Test Started! " + Thread.currentThread().getId());
         driver.navigate().to("http://www.google.com");
         System.out.println("Google3 Test's Page title is: " + driver.getTitle() + " " + Thread.currentThread().getId());
         Assert.assertEquals(driver.getTitle(), "Google");
         System.out.println("Google3 Test Ended! " + Thread.currentThread().getId());
-
     }
 
     @Test
-    public  void GOOGLE4() throws Exception {
+    public void GOOGLE4() throws Exception {
         System.out.println("Google4 Test Started! " + Thread.currentThread().getId());
         driver.navigate().to("http://www.google.com");
         System.out.println("Google3 Test's Page title is: " + driver.getTitle() + " " + Thread.currentThread().getId());
         Assert.assertEquals(driver.getTitle(), "Google");
         System.out.println("Google3 Test Ended! " + Thread.currentThread().getId());
-
     }
 
 }
