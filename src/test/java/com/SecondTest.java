2dbd8f4af241b666008d0a0724af5ab58785a023
SW Test Academy <swtestacademy@gmail.com>
2017-11-10 01:19:05 +0300

    minor changes.

--- src/test/java/com/SecondTest.java
+++ src/test/java/com/SecondTest.java
@@ -9,20 +9,16 @@ import org.testng.annotations.Test;
 public class SecondTest extends TestBase{
 
     @Test
-    public  void GOOGLE1() throws Exception {
-
+    public void GOOGLE1() throws Exception {
         System.out.println("Google1 Test Started! " + Thread.currentThread().getId());
         driver.navigate().to("http://www.google.com");
         System.out.println("Google1 Test's Page title is: " + driver.getTitle() + " " + Thread.currentThread().getId());
         Assert.assertEquals(driver.getTitle(), "Google");
         System.out.println("Google1 Test Ended! " + Thread.currentThread().getId());
-
     }
 
     @Test
-    public  void YANDEX() throws Exception {
-
-
+    public void YANDEX() throws Exception {
         System.out.println("Yandex Test Started! " + Thread.currentThread().getId());
         driver.navigate().to("http://www.yandex.com");
         System.out.println("Yandex Test's Page title is: " + driver.getTitle() + " " + Thread.currentThread().getId());
